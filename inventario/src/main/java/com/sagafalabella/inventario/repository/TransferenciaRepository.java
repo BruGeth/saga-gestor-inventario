6d11a114e9a931c38f337d935759abb7bf5b66ac
Ashlee <jaeash26@gmail.com>
2025-07-22 22:28:27 -0500

    feat: agregar repositorios para Transferencia, Usuario y Zona

---
+++ inventario/src/main/java/com/sagafalabella/inventario/repository/TransferenciaRepository.java
@@ -0,0 +1,7 @@
+package com.sagafalabella.inventario.repository;
+
+import com.sagafalabella.inventario.model.Transferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferenciaRepository extends JpaRepository<Transferencia, Integer> {
+}
---
+++ inventario/src/main/java/com/sagafalabella/inventario/repository/ZonaRepository.java
@@ -0,0 +1,7 @@
+package com.sagafalabella.inventario.repository;
+
+import com.sagafalabella.inventario.model.Zona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZonaRepository extends JpaRepository<Zona, Integer> {
+}
