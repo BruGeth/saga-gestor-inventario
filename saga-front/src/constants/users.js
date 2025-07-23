b747034a8ec55ddf6756929b1d6a473a7181db13
BruGeth <g15bru03@hotmail.com>
2025-07-22 22:41:11 -0500

    feat: Add MOCK_USERS and ROLES constants for user management

---
+++ saga-front/src/constants/users.js
@@ -0,0 +1,32 @@
+export const MOCK_USERS = [
+  {
+    id: 1,
+    nombre: "Ashlee",
+    apellido: "Garcia",
+    email: "ash.gar@falabella.com",
+    rol: "ADMINISTRADOR",
+    avatar: "AG",
+  },
+  {
+    id: 2,
+    nombre: "Angely",
+    apellido: "Corahua",
+    email: "an.corahua@falabella.com",
+    rol: "ASISTENTE_LOGISTICA",
+    avatar: "AC",
+  },
+  {
+    id: 3,
+    nombre: "Bruno",
+    apellido: "Guerra",
+    email: "bru.guerra@falabella.com",
+    rol: "SUPERVISOR",
+    avatar: "BG",
+  },
+]
+
+export const ROLES = {
+  ADMINISTRADOR: "ADMINISTRADOR",
+  SUPERVISOR: "SUPERVISOR",
+  ASISTENTE_LOGISTICA: "ASISTENTE_LOGISTICA",
+}
