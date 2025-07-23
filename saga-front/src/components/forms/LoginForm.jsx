c2cb506cf39eafde200c370f042d8bad30c1b559
BruGeth <g15bru03@hotmail.com>
2025-07-22 22:44:41 -0500

    feat: Replace SVG icons with font icons in LoginForm component for improved performance and consistency

--- saga-front/src/components/forms/LoginForm.jsx
+++ saga-front/src/components/forms/LoginForm.jsx
@@ -34,20 +34,7 @@ function LoginForm({ onLogin }) {
       <div className="login-card">
         <div className="login-header">
           <div className="login-logo">
-            <svg
-              className="login-logo-icon"
-              fill="none"
-              stroke="white"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-3 4h2a1 1 0 001-1v-1a1 1 0 00-1-1h-2a1 1 0 00-1 1v1a1 1 0 001 1z"
-              />
-            </svg>
+            <i className="icon-building"></i>
           </div>
           <h2 className="login-title">Sistema de Inventario</h2>
           <p className="login-subtitle">Saga Falabella - Mallplaza Comas</p>
@@ -57,20 +44,7 @@ function LoginForm({ onLogin }) {
             <div className="form-group">
               <label htmlFor="email">Email</label>
               <div className="input-with-icon">
-                <svg
-                  className="input-icon"
-                  fill="none"
-                  stroke="#a0aec0"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  />
-                </svg>
+                <i className="icon-user"></i>
                 <input
                   id="email"
                   type="email"
@@ -84,20 +58,7 @@ function LoginForm({ onLogin }) {
             <div className="form-group">
               <label htmlFor="password">Contraseña</label>
               <div className="input-with-icon">
-                <svg
-                  className="input-icon"
-                  fill="none"
-                  stroke="#a0aec0"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                  />
-                </svg>
+                <i className="icon-lock"></i>
                 <input
                   id="password"
                   type="password"
